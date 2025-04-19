70dd62c1b7a5a8476a8a1ca498bfff8dfc4ba845
triplemzim <triplemzim@gmail.com>
2025-04-19 22:12:54 +0800

    increase smtp timeout to 30s

--- emaily/views.py
+++ emaily/views.py
@@ -44,7 +44,7 @@ def check_email(email):
         return "invalid", "no_mx"
 
     try:
-        server = smtplib.SMTP(timeout=10)
+        server = smtplib.SMTP(timeout=30)
         server.connect(mx_record)
         server.helo("example.com")
         server.mail("probe@example.com")
@@ -57,7 +57,7 @@ def check_email(email):
 
     def smtp_check():
         try:
-            server = smtplib.SMTP(timeout=20)
+            server = smtplib.SMTP(timeout=30)
             server.connect(mx_record)
             server.helo("example.com")
             server.mail("verifier@example.com")
